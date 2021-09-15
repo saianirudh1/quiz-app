bc60cc1bb17892caae54b42a886471d6b62b68d5
Sai Anirudh <b.anirudh10@gmail.com>
2021-09-16 03:48:44 +0530

    Added Intro Activity

---
+++ app/src/main/java/com/example/quizz/IntroActivity.java
@@ -0,0 +1,34 @@
+package com.example.quizz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+public class IntroActivity extends AppCompatActivity {
+
+    TextView introText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        introText = findViewById(R.id.textViewIntro);
+        Animation introAnimation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.intro);
+        introText.setAnimation(introAnimation);
+
+        Intent intent = new Intent(this, MainActivity.class);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(intent);
+            }
+        }, 2700);
+    }
+}
